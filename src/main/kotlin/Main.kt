3de97525e1e4850e7a7ed9500553fda6de1d331d
ARDILO <ardilobrt@gmail.com>
2022-04-23 20:59:05 +0700

    Fixed Class & Object Fixed calling object from class Player

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,18 +4,21 @@ fun main() {
     println("GAME SUIT TERMINAL VERSION")
     showSymbol(25)
 
+    //PLAYER 1
     print("Player-1 Input Pilihan Anda(gunting/batu/kertas): ")
     val inputPlayer1:String = readLine()!!
-    val player1 = Player(inputPlayer1)
-    player1.getNameP1()
+    val player1 = Player("Player-1")
+    player1.handPlayer = inputPlayer1.filter { !it.isWhitespace() }
     if (player1.isCheckHand()) return
 
+    //PLAYER 2
     print("Player-2 Input Pilihan Anda(gunting/batu/kertas): ")
     val inputPlayer2:String = readLine()!!
-    val player2 = Player(inputPlayer2)
-    player2.getNameP2()
+    val player2 = Player("Player-2")
+    player2.handPlayer = inputPlayer2.filter { !it.isWhitespace() }
     if (player2.isCheckHand()) return
 
+    //Game Start
     val game = Game(player1,player2)
     game.startGame()
 }
